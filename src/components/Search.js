a778097f91659a0491e1763c1f70555b9d94c2c5
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2023-11-02 14:23:07 -0400

    Update Search.js
    Only added the fuctionality of the Search method. Styling still needs to be done

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,43 @@
-function Search(){
-    return (
-        <div>Search</div>
-    )
+import { Input, Select, Button } from 'antd';
+//This is only the fuctionality, styling still needs to be done. Hence it is still incomplete
+const { Search } = Input;
+
+function Search() {
+
+  const [searchText, setSearchText] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  const handleSearch = () => {
+    // put search API here
+  }
+
+  return (
+    <div>
+      <Search 
+        placeholder="Search keywords"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+
+      <Select 
+        defaultValue="all" 
+        onChange={value => setFilter(value)}
+      >
+        <Option value="all">All</Option>  
+        <Option value="text">Text Only</Option>
+        <Option value="images">With Images</Option>
+        <Option value="videos">With Videos</Option>
+      </Select>
+
+      <Button 
+        type="primary"
+        onClick={handleSearch}  
+      >
+        Search
+      </Button>
+    </div>
+  )
+
 }
 
 export default Search;
