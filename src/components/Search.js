0b1ea7f367925763b200b62b58029dfe45941f19
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2023-11-03 23:27:31 -0400

    Update Search.js
    styling and functionality complete, tested on my end. const handleSearch is left empty for if we'd be using a search API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,5 @@
-import { Input, Select, Button } from 'antd';
+import { Input, Select, Button, Card } from 'antd';
 import { useState } from 'react';
-//This is only the fuctionality, styling still needs to be done. Hence it is still incomplete
 const { Search } = Input;
 
 function SearchPage () {
@@ -13,29 +12,39 @@ function SearchPage () {
   }
 
   return (
-    <div>
-      <Search 
-        placeholder="Search keywords"
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-      />
+    <div className="h-full w-full flex justify-center items-center">
+      <div className="w-8/12 h-full">
+        <Card className="w-full h-52 border border-slate-200 drop-shadow mt-7">
+          <div className="w-full h-full flex flex-col gap-10 justify-center">
+            <Search 
+              className="w-full h-24 resize-none"
+              placeholder="Search keywords"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <div className="flex justify-between w-full">
+            <Select 
+              className="flex w-2/12 gap-2"
+              defaultValue="all" 
+              onChange={value => setFilter(value)}
+            >
+              <option value="all">All</option>  
+              <option value="text">Text Only</option>
+              <option value="images">Images</option>
+              <option value="videos">Videos</option>
+            </Select>
 
-      <Select 
-        defaultValue="all" 
-        onChange={value => setFilter(value)}
-      >
-        <option value="all">All</option>  
-        <option value="text">Text Only</option>
-        <option value="images">With Images</option>
-        <option value="videos">With Videos</option>
-      </Select>
-
-      <Button 
-        type="primary"
-        onClick={handleSearch}  
-      >
-        Search
-      </Button>
+            <Button 
+              className="px-5 bg-[#4096FF] text-white font-bold"
+              onClick={handleSearch}  
+            >
+              Search
+            </Button>
+            </div>
+            
+          </div>
+        </Card>
+      </div>
     </div>
   )
 
