4560ba644ff78a3814dbc1234ddc1e6f49317e22
rafi-d-z <rafidizia@gmail.com>
2023-11-19 16:49:26 -0500

    updated like buttons for profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,16 @@
 import { LikeOutlined, DislikeOutlined } from "@ant-design/icons";
-import { Avatar, Card, } from "antd";
+import { Avatar, Button, Card, } from "antd";
+
+const {Meta} = Card;
 
 function Profile(){
-    const {Meta} = Card;
-
+    const likes = 0;
     const pfp = "https://i.pinimg.com/originals/d8/f5/2c/d8f52ce52985768ccac65f9550baf49e.jpg"; 
 
     return(
         <div className="h-full w-full flex justify-center items-center">
             <div className="w-8/12 h-full">
-                <Card className="w-full h-fit mt-10" bordered={false}>
+                <Card className="w-full h-fit mt-7" bordered={false}>
                     <Meta
                     avatar={<Avatar src={pfp} size={75}/>}
                     title="User Name"
@@ -23,7 +24,7 @@ function Profile(){
                     <p>Profile Bio</p>
                 </Card>
 
-                <Card className="w-2/5 h-fit mt-10" style={{border:"0.05rem solid rgb(229, 229, 229)"}}>
+                <Card className="w-2/5 h-fit mt-7" style={{border:"0.05rem solid rgb(229, 229, 229)"}}>
                     <Meta
                     avatar={<Avatar src={pfp} size={50}/>}
                     title="User Name"
@@ -37,7 +38,7 @@ function Profile(){
                     <p>wealth fame power, gol d roger, the king of pirates, has obtained these and everything else the world has to offer</p>
                     <br/>
                     <div style={{paddingLeft: '80%'}}>
-                        <LikeOutlined style={{fontSize:'20px'}}/>   8008   <DislikeOutlined style={{fontSize:'20px'}}/>
+                        <Button type='text' icon={<LikeOutlined/>}/> {likes} <Button type='text' icon={<DislikeOutlined/>}/>
                     </div>
                 </Card>
             </div>
