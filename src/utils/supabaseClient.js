3a2b8638a1e409edec6acba48b6e1c6815823682
rafi-d-z <rafidizia@gmail.com>
2023-12-03 23:34:25 -0500

    hid api key

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient  } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://gopjsvqjoeoawvccsgax.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvcGpzdnFqb2VvYXd2Y2NzZ2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk5ODY0MzMsImV4cCI6MjAxNTU2MjQzM30.k-E_0hyP-WtkVshxCRGGkVN7V9IUn9BTtjc-nQU7jeQ'
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
