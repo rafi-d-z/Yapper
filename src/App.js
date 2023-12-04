f76f2f02d5baf5850886845ef4ab0294f6f58028
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2023-12-03 20:57:26 -0500

    Update App.js
    Added route for feed.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Auth from "./pages/Auth";
 import LayoutPage from "./pages/Layout";
+import Feed from "./components/Feed";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-          <Route path='/' element={<LayoutPage />} />
-          <Route path="/auth" element={<Auth />} />
-      </Routes>
+      <Route path='/' element={<LayoutPage />} />
+      <Route path='/feed' element={<Feed />} /> 
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   );
 }
 
