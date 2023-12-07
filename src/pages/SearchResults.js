ab900d36484f556826791101043489ab64cc5fda
Henry <hsuarez000@citymail.cuny.edu>
2023-12-07 08:43:08 -0500

    made changes to query

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -134,6 +134,7 @@ const handleSubscribe = async () => {
   try {
     const { data: subscriptionData, error: subscriptionError } = await supabase
       .from('subscribers')
+      .match({user_i : myUser.id ,follow_id : user.id})
       .select('user_id', myUser.id)
       .eq ('follow_id', user.id)
 
